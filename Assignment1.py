c91e36bc5ac1893f60e329e6a79d44327ce51863
smit2210 <56782772+smit2210@users.noreply.github.com>
2025-05-07 16:42:31 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,10 @@
+from xml.dom.minidom import ProcessingInstruction
+
+a= input('Enter the first number: ')
+b= input('Enter the second number: ')
+a=int(a)
+b=int(b)
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
---
+++ Assignment2.py
@@ -0,0 +1,7 @@
+from xml.dom.minidom import ProcessingInstruction
+
+a=input('Enter the first name: ')
+b=input('Enter the last name: ')
+a=str(a)
+b=str(b)
+print('Hello,',a , b,'! Welcome to Python Program.')
